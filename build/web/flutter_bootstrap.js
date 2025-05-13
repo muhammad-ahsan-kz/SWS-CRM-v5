fd89a06ce822d509ac8d5242650f5c661cbd414a
muhammad-ahsan-kz <muhammad.ahsan@skynetsilicon.com>
2025-05-13 09:18:17 +0500

    Updated

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "266579155"
+    serviceWorkerVersion: "2858591674"
   }
 });
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:sws_crm_v5/home_page.dart';
+import 'package:sws_crm_v5/views/home_page.dart';
 
 void main() {
   runApp(const MyApp());
